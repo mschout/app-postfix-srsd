5c640451548c06b9d84379978c8a8b6a07e90610
Michael Schout <mschout@gkg.net>
2014-04-24 13:41:42 -0500

    add stress test script

---
+++ tools/stress.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+#
+
+my $verbose = 1;
+
+use Modern::Perl '2013';
+use Parallel::ForkManager;
+use IO::Socket;
+use Text::Netstring qw(netstring_read netstring_encode netstring_decode);;
+
+my ($socket, $cycles) = @ARGV or die "usage: $0 socket iterations\n";
+
+my @forward = (map { "mschout$_\@gkg.net" } (1 .. $cycles));
+
+my $pm = Parallel::ForkManager->new(16);
+
+for my $req (@forward) {
+    $pm->start and next;
+
+    process(srsencoder => $req);
+
+    $pm->finish;
+}
+
+$pm->wait_all_children;
+
+sub process {
+    my ($type, $email) = @_;
+
+    my $sock = IO::Socket::UNIX->new(Type => SOCK_STREAM, Peer => $socket) or die "socket: $!";
+
+    print $sock netstring_encode("$type $email");
+    my $reply = netstring_decode(netstring_read($sock));
+    $reply =~ s/^.* //;
+
+    say "$email -> $reply" if $verbose;
+
+    return unless defined $reply and length $reply > 0;
+
+    if ($type eq 'srsencoder') {
+        process(srsdecoder => $reply);
+    }
+}
+
+
+
